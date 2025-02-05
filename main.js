8a433c7bdf477850dda6e5c85873a095f7dbde76
IxtiyorAnarboyev <anarboevixtiyor005@gmail.com>
2025-02-05 16:58:18 +0500

    JS 1-dars
    Zeromax IT center

---
+++ main.js
@@ -0,0 +1,40 @@
+// JSga kirish
+
+// console.log("Men faqat konsol oynasida ko'rinaman!");
+// alert("Saytimizga xush kelibsiz!")
+// document.write("Men bodiyda ko'rinaman!")
+
+// JS o'zgaruvchilari
+
+// let xson = 5;
+// let yson = 7;
+// let zson = 10;
+// document.write(xson+yson+zson)
+
+// JS Ma'lumotlar turlari
+
+// let son = 45;
+// let matn = "JavaScript";
+// let matnSon = "45"; 
+
+// document.write(typeof matnSon);
+
+// JSda matematik amallarning ishlashi
+
+let x = 61;
+
+ // x+=5;
+//  x-11;
+// x*5;
+// x/22;
+// let daraja = x**5;
+
+// ++x;
+// x++;
+// --x;
+
+
+ document.write(x);
+
+
+
